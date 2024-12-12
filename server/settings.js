48ec9c70ccca54db21d3c829722d8153323b4e55
rcutte <rcutte@student.42.fr>
2024-12-12 14:33:41 +0100

    Refactor:  seed function to drop and create new database tables for tags, chats, messages, and views

--- server/settings.js
+++ server/settings.js
@@ -18,6 +18,9 @@ async function seed() {
   try {
     await pool.connect();
 
+    await pool.query("DROP TABLE IF EXISTS tags;");
+    await pool.query("DROP TABLE IF EXISTS chats;");
+    await pool.query("DROP TABLE IF EXISTS msgs;");
     await pool.query("DROP TABLE IF EXISTS users;");
 
     /*USERS TABLE*/
@@ -80,7 +83,7 @@ async function seed() {
 
     /*INTERESTS TAGS TABLE*/
     const createInterestsTagTableQuery = `
-      CREATE TABLE IF NOT EXISTS interests_tag (
+      CREATE TABLE IF NOT EXISTS tags (
         id SERIAL PRIMARY KEY,
         tag VARCHAR(100) UNIQUE NOT NULL
       );
@@ -88,7 +91,7 @@ async function seed() {
     await pool.query(createInterestsTagTableQuery);
 
     const insertTagsQuery = `
-      INSERT INTO interests_tag (tag)
+      INSERT INTO tags (tag)
       VALUES
         ('art'),
         ('photography'),
@@ -122,11 +125,44 @@ async function seed() {
     `;
     await pool.query(insertTagsQuery);
 
-    /*MESSAGES TABLE*/
+    
     /*CHAT TABLE*/
-    /*VIEWS HISTORY TABLE*/
-    /*REPORTED USERS TABLE*/
+    const createChatQuery = `
+     CREATE TABLE IF NOT EXISTS chats (
+        id SERIAL PRIMARY KEY,
+        user_1 INT REFERENCES users(id),
+        user_2 INT REFERENCES users(id),
+        deleted_by INT[]
+      );
+    `
+    await pool.query(createChatQuery);
+   
+    /*MESSAGES TABLE*/
+    const createMsgQuery = `
+        CREATE TABLE IF NOT EXISTS msgs (
+        id SERIAL PRIMARY KEY,
+        content VARCHAR(100) NOT NULL,
+        chat_id INT REFERENCES chats(id),
+        sender_id INT REFERENCES users(id),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `;
+    await pool.query(createMsgQuery);
 
+    /*VIEWS TABLE*/
+    const createViewsQuery = `
+        CREATE TABLE IF NOT EXISTS views (
+            id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(id),
+            chat_id INT REFERENCES chats(id),
+            viewed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    `;
+    await pool.query(createViewsQuery);
+
+    /*VIEWS HISTORY TABLE*/
+    
+    /*REPORTED USERS TABLE*/
   } catch (err) {
     console.error("Error seeding the database:", err);
   } finally {
