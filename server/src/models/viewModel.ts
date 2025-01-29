c3d9a39bae69183fc51be511ea812a9764ce8c0a
rcutte <rcutte@student.42.fr>
2025-01-29 12:15:16 +0100

    Refactor: rm double view count at view creation

--- server/src/models/viewModel.ts
+++ server/src/models/viewModel.ts
@@ -25,10 +25,6 @@ class ViewModel {
   }
 
   async addUserView(userId: number, viewedUserId: number): Promise<boolean> {
-    // Future improvement:
-    // - improve the logic to prevent spamming views
-    // (current time check is not enough - buggy double request at exact same time with debug tools)
-
     const lastViewTime = await this.getLastViewTime(userId, viewedUserId);
 
     if (lastViewTime) {
@@ -39,15 +35,46 @@ class ViewModel {
       }
     }
 
+    // const query = {
+    //   text: `
+    //     INSERT INTO views (viewer_user_id, viewed_user_id, view_count, last_viewed_at)
+    //     VALUES ($1, $2, 1, NOW())
+    //     ON CONFLICT (viewer_user_id, viewed_user_id)
+    //     DO UPDATE SET
+    //       view_count = views.view_count + 1,
+    //       last_viewed_at = NOW()
+    //   `,
+    //   values: [userId, viewedUserId],
+    // };
+
+    // Optimized query to prevent too many updates of view_count and last_viewed_at for a short period of time
     const query = {
       text: `
-        INSERT INTO views (viewer_user_id, viewed_user_id, view_count, last_viewed_at)
-        VALUES ($1, $2, 1, NOW())
-        ON CONFLICT (viewer_user_id, viewed_user_id)
-        DO UPDATE SET 
-          view_count = views.view_count + 1,
-          last_viewed_at = NOW()
-      `,
+      WITH last_view AS (
+        SELECT last_viewed_at 
+        FROM views 
+        WHERE viewer_user_id = $1 
+        AND viewed_user_id = $2
+      )
+      INSERT INTO views (viewer_user_id, viewed_user_id, view_count, last_viewed_at)
+      SELECT $1, $2, 1, NOW()
+      WHERE NOT EXISTS (
+        SELECT 1 FROM last_view 
+        WHERE last_viewed_at > NOW() - INTERVAL '1 minute'
+      )
+      ON CONFLICT (viewer_user_id, viewed_user_id)
+      DO UPDATE SET 
+        view_count = CASE 
+          WHEN views.last_viewed_at < NOW() - INTERVAL '1 minute'
+          THEN views.view_count + 1 
+          ELSE views.view_count 
+        END,
+        last_viewed_at = CASE 
+          WHEN views.last_viewed_at < NOW() - INTERVAL '1 minute'
+          THEN NOW() 
+          ELSE views.last_viewed_at 
+        END
+    `,
       values: [userId, viewedUserId],
     };
 
